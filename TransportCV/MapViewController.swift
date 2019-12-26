f44dcf66156377a5e579719f175652369b6e1275
Stanislav Shemiakov <stas2992@gmail.com>
2019-12-26 15:08:25 +0200

    iOS 9 compatibility setup, Map view template

---
+++ TransportCV/MapViewController.swift
@@ -0,0 +1,116 @@
+//
+//  MapViewController.swift
+//  TransportCV
+//
+//  Created by Stanislav on 25.12.2019.
+//  Copyright © 2019 OrbitalMan. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+    
+    /// The main view. Represents a map to pick a location on.
+    let mapView = MKMapView()
+    
+    /// The `MKPointAnnotation` indicating current fake location on the `mapView`.
+    let pointer = MKPointAnnotation()
+    
+    /// Requests the real location to get initial coordinate for user convenience.
+    let locationManager = CLLocationManager()
+    
+    //MARK: -
+    
+    /// Sets the `mapView`, text fields and navigation items up.
+    override func loadView() {
+        super.loadView()
+        title = "Map"
+        view = mapView
+    }
+    
+    /// The `mapView` and `locationManager` setup.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        mapView.addAnnotation(pointer)
+        mapView.delegate = self
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyThreeKilometers
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        getLocation()
+    }
+    
+    //MARK: -
+    
+    /// Asks the `locationManager` to request initial location.
+    /// Also does `requestWhenInUseAuthorization` if necessary.
+    func getLocation() {
+        // No need to ask a location in this case - it will default (or did) to `AppConfig.location.customCoordinate` (see the `viewDidLoad`)
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedAlways, .authorizedWhenInUse:
+            if CLLocationManager.locationServicesEnabled() {
+                locationManager.requestLocation()
+            }
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .denied, .restricted:
+            break
+        @unknown default:
+            break
+        }
+    }
+    
+    /// Moves visible region of the `mapView` to given location.
+    ///
+    /// - Parameters:
+    ///   - newCoordinate: The `CLLocationCoordinate2D` to move to.
+    ///   - size: The span size in `CLLocationDegrees`. Will use the `mapView` span if `nil` passed.
+    ///   - animated: Whether it will be animated transition.
+    func updateLocation(newCoordinate: CLLocationCoordinate2D,
+                         size: CLLocationDegrees? = nil,
+                         animated: Bool) {
+        let span: MKCoordinateSpan
+        if let size = size {
+            span = MKCoordinateSpan(latitudeDelta: size,
+                                    longitudeDelta: size)
+        } else {
+            span = mapView.region.span
+        }
+        mapView.setRegion(MKCoordinateRegion(center: newCoordinate,
+                                             span: span),
+                          animated: animated)
+    }
+    
+}
+
+// MARK: - MKMapViewDelegate
+extension MapViewController: MKMapViewDelegate {
+    
+    func mapViewDidChangeVisibleRegion(_ mapView: MKMapView) {
+        pointer.coordinate = mapView.centerCoordinate
+    }
+    
+}
+
+// MARK: - CLLocationManagerDelegate
+extension MapViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        getLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let userCoordinate = locations.last?.coordinate {
+            updateLocation(newCoordinate: userCoordinate, size: 0.05, animated: true)
+        }
+        manager.stopUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("locationManager didFailWithError: \(error.localizedDescription)")
+    }
+    
+}
